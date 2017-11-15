fa20330f1f4dda793edce8398cd486257a7e2196
郑懿彬 <lance.zyb@gmail.com>
2017-11-15 14:57:11 +0800

    个人中心头像

--- src/pages/my.vue
+++ src/pages/my.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="post">
-      www
+  <div class="my">
+      <p id='my-avatar'>
+        <MyAvatar />
+      </p>
   </div>
 </template>
 
 <script>
+import MyAvatar from '@/components/myAvatar.vue';
 export default {
   name: 'post',
+  components: { MyAvatar },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -14,6 +18,8 @@ export default {
   }
 }
 </script>
-<style scoped>
-
+<style lang="scss">
+#my-avatar {
+  margin-top: .5rem;
+}
 </style>
